3b5141336987dc71cea29e2d116f85bed2b74bf3
Brian Salchert <brisalchert@gmail.com>
2025-03-20 20:09:13 -0400

    Adjust output format for expected value testing

--- src/ExpectedValues.java
+++ src/ExpectedValues.java
@@ -9,11 +9,13 @@ public class ExpectedValues {
         // Initialize a new game with player hand [10, 6] and up card [10].
         int up = 10;
         int playerCard1 = 10;
-        int playerCard2 = 7;
+        int playerCard2 = 6;
 
         Blackjack game = new Blackjack(up, playerCard1, playerCard2);
 
-        System.out.println(game.expectedValueHit());
-        System.out.println(game.expectedValueStand());
+        System.out.println("Up Card:\t" + up);
+        System.out.println("Hand:\t\t" + playerCard1 + ", " + playerCard2);
+        System.out.println("Hit:\t\t" + game.expectedValueHit());
+        System.out.println("Stand:\t\t" + game.expectedValueStand());
     }
 }
